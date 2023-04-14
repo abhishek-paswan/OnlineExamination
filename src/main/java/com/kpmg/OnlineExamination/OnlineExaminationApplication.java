cac3442feb2f1b5618519b50ce25e7662a8a8402
abhi816109 <abhi816109@gmail.com>
2023-04-14 12:48:36 +0530

    Initialize Online Examination Project

---
+++ src/main/java/com/kpmg/OnlineExamination/OnlineExaminationApplication.java
@@ -0,0 +1,13 @@
+package com.kpmg.OnlineExamination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExaminationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExaminationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpmg/OnlineExamination/OnlineExaminationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpmg.OnlineExamination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExaminationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
